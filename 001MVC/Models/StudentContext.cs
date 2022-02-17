84eceae769bcd23b62d3ff751591a61487efc2af
parshev <s.parshev19@gmail.com>
2022-02-17 14:04:36 +0200

    initial project commit

---
+++ 001MVC/Models/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _001MVC.Models
+{
+    public class StudentContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=StudentDB; Trusted_Connection = true");
+        }
+    }
+}
